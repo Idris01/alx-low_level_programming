7b57c1f60e6ef7b71f84723f82267da3a0e06854
root <idrys01@gmail.com>
2023-02-16 06:07:11 -0800

    C program that print primitive sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entrypoint for program
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %lu bytes(s)\n", sizeof(c));
+
+	printf("Size of an int: %lu bytes(s)\n", sizeof(i));
+
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(li));
+
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(lli));
+
+	printf("Size of a float: %lu bytes(s)\n", sizeof(f));
+	return (0);
+}
