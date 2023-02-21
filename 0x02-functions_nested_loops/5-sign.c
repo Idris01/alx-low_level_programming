b6ed63d9d491eef23bf2a24a14c94b011c53cd85
root <idrys01@gmail.com>
2023-02-21 08:08:14 -0800

    Print sign of given number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a given number n
+ * @n : Number to print corresponding sign
+ * Description: If n == 0, '0' is printed and 0
+ * returned, if n > O, '+' is printed and '1' is
+ * returned otherwise '-' is printed and '-1' returned
+ *
+ * Return: 1 , -1 or 0
+ */
+int print_sign(int n)
+{
+	if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,4 @@ void _putchar(char data);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
