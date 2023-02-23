beb2826e06905a036ae1289f596bc67347296865
root <idrys01@gmail.com>
2023-02-23 09:33:49 -0800

    Prints a given integer number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+void print_it(int n);
+
+/**
+ * print_number - Prints any integer number
+ * @n: Number to print
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	print_it(n);
+	/*_putchar('\n');*/
+}
+
+/**
+ * print_it - Helper function to print a positive  number
+ * @n: Number to print
+ *
+ * Return: void
+ */
+
+void print_it(int n)
+{
+	int rem = n % 10;
+	int div = n / 10;
+
+	if (div == 0)
+	{
+		_putchar('0' + rem);
+		return;
+	}
+	print_it(div);
+	_putchar('0' + rem);
+}
