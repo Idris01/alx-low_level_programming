eec4d29043e49ccb67829437478860ac29c62a5f
root <idrys01@gmail.com>
2023-02-23 04:31:56 -0800

    Add header file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,10 @@
 #include <ctype.h>
-
 /**
  * _isupper - Check if a given character is Uppercase
  * @c: Character to check
  *
  * Return: 1 if True and 0 otherwise
  */
-
 int _isupper(int c)
 {
 	if (isupper(c))
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void _putchar(int c);
+void print_most_numbers(void);
+void more_numbers(void);
+
