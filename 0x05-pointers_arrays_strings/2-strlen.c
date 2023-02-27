9787831b814f26c20da9d3de8fc9e78f047182c0
root <idrys01@gmail.com>
2023-02-27 02:38:18 -0800

    Add program to calculate length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strlen - find and return length of string
+ * @s: a string to find length of
+ *
+ * Return: integer
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
