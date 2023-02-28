c12fa6b5019e76738d4c03931b215cab7cc99729
root <idrys01@gmail.com>
2023-02-28 11:58:24 -0800

    Fix: adjust negative calculation

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,44 +11,43 @@
 
 int _atoi(char *s)
 {
-	int value, size = strlen(s) - 1;
+	int value, neg = 0, size = strlen(s) - 1;
 	int start, end, iterator = 0, copySize = 0;
 	char *copy;
 
 	while (iterator < size)
 	{
-		if (isdigit(*(s + iterator)))
+		if (*(s + iterator) == '-')
+			neg++;
+		else if (isdigit(*(s + iterator)))
 			break;
 		iterator++;
 	}
 	if (!isdigit(*(s + iterator)))
 		return (0);
-
-	if (iterator > 0 && s[iterator - 1] == '-')
-		start = iterator - 1;
-	else
-		start = iterator;
-
+	start = iterator;
 	while (iterator < size)
 	{
 		if (!isdigit(*(s + iterator)))
 			break;
 		iterator++;
 	}
-
 	if (isdigit(*(s + iterator)))
 	{
 		copySize = (size - start) + 1;
 		copy = malloc(copySize * sizeof(char));
 		strncpy(copy, (s + start), copySize);
 		value = atoi(copy);
+		if (neg % 2 == 1)
+			value = -value;
 		return (value);
 	}
-
 	end = iterator - 1;
 	copySize = (end - start) + 1;
 	copy = malloc(copySize * sizeof(char));
 	strncpy(copy, (s + start), copySize);
 	value = atoi(copy);
+	if (neg % 2 == 1)
+		value = -value;
 	return (value);
 }
