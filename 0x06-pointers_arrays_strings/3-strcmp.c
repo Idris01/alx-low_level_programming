acce0b4d76e5180fb7e4c59f46fe07d749725e69
root <idrys01@gmail.com>
2023-03-01 23:50:45 -0800

    Add _strcmp function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strcmp - Compares two string s1 and s2
+ * Return the difference between the ASCII of
+ * the first non matching character in s1 and s2
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
