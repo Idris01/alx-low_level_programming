a82c42ee0b1d5dfa8e195670c39c7cad3279df0e
root <idrys01@gmail.com>
2023-03-06 06:21:41 -0800

    Add README file, main.h header file and memset file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ * _memset - Set the first n address of given
+ * string s to b
+ * @s: string to set change values of
+ * @b: character constant value
+ * @n: integer number of addresses to set to b
+ *
+ * Return: pointer to string s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int counter = 0;
+
+	while (counter < n)
+	{
+		*(s + counter++) = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+# Pointer To Pointer
+This project deals with tasks involving pointer to pointer in C programming Language
