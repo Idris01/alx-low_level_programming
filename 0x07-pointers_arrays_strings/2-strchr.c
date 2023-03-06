034290eaede6130b87b969117f399925d498023a
root <idrys01@gmail.com>
2023-03-06 07:20:18 -0800

    Add strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+
+/**
+ * _strchr - Find the first occurence of character
+ * c in string s
+ * @s: pointer to string
+ * @c: character to find
+ *
+ * Return: pointer to address of c in s otherwise
+ * NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int locator = 0;
+	char itm;
+
+	while ((itm = *(s + locator)) != '\0')
+	{
+		if (itm == c)
+			return (s + locator);
+		locator++;
+	}
+	return (NULL);
+}
