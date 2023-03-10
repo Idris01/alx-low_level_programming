142fdb11c7813ae1f313c27e278f8052ebef4796
root <idrys01@gmail.com>
2023-03-10 08:14:18 -0800

    Add README file, main header file and O-whatsmyname program file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint
+ * Prints the program name
+ * @argc: integer number of commandline arguments
+ * @argv: array of commandline values
+ *
+ * Return: integer Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+# Understanding argc and argv in C Programming
+This project deals with tasks involving arcc and argv
