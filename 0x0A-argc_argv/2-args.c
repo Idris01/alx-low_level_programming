18a15de9176917da61497a8c7aec83a444f4222d
root <idrys01@gmail.com>
2023-03-10 08:40:22 -0800

    Program that prints all command line arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint to program
+ * Prints all commandline Arguments of the progra
+ * @argc: integer number of arguments
+ * @argv: array of arguments
+ *
+ * Return: integer Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int num = 0;
+
+	while (num < argc)
+	{
+		printf("%s\n", *(argv + num));
+		num++;
+	}
+}
