a86b9b4e8e2398b0cb18e8d0e5178b4fe610a135
root <idrys01@gmail.com>
2023-03-20 06:12:28 -0700

    Add README and dog.h header file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# Struct and Typedef in C
+The project consists of tasks involving struct and typedef in c programming
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - Define a structure named dog
+ * @name: pointer to string
+ * @age: integer
+ * @owner: pointer to string
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
