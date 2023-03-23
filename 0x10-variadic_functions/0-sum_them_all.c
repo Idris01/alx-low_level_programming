200a3c0d8bf2ab033b6234bb77f942c7c98112d7
root <idrys01@gmail.com>
2023-03-23 07:12:37 -0700

    Add README file, 0-sum_them_all source file amd header file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Sum given n number of integers
+ * @n: integer number of arguments
+ *
+ * Return: integer sum of all arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list items;
+	int i, sum;
+
+	if (n == 0)
+		return (0);
+	va_start(items, n);
+	sum = 0;
+	i = 0;
+	for (; i < n; i++)
+		sum += va_arg(items, int);
+	va_end(items);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+# Variadic Functions in C Programming
+This project consists of tasks involving variadic functions in C programming language
