463bdc55d7cdaab29aea5c4bb9d3ef91b4fa7371
root <idrys01@gmail.com>
2023-03-30 12:05:43 -0700

    Add function the run before main entry point of program:wq

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - Run before main entry point of moon
+ *
+ * Return: void
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet,you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
