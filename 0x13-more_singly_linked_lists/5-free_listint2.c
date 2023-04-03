cf7776cb4e64cfb5171889e8b56c1622b293eef3
root <idrys01@gmail.com>
2023-04-03 02:37:45 -0700

    Add function that free storage allocated to a linked list and set the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Free the memory allocated to an
+ * integer linkedlist then set the head to null
+ * @head: pointer to head node of the linkedlist
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL && (*head)->next != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	free(*head);
+	*head = NULL;
+}
