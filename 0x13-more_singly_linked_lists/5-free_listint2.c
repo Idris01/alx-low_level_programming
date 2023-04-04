25ef3364cfea2a2aa211d10af787738ac2ea4ede
root <idrys01@gmail.com>
2023-04-04 09:11:14 -0700

    update null feedback message

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "lists.h"
 
+void print_string(char *s);
+
 /**
  * free_listint2 - Free the memory allocated to an
  * integer linkedlist then set the head to null
@@ -13,6 +15,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (*head == NULL)
+	{
+		print_string("Freed !\n");
+		exit(0);
+	}
 	while (*head != NULL && (*head)->next != NULL)
 	{
 		temp = *head;
@@ -21,5 +28,19 @@ void free_listint2(listint_t **head)
 	}
 	free(*head);
 	*head = NULL;
-	head = NULL;
+}
+
+/**
+ * print_string - Print given string to stdout
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+
+void print_string(char *s)
+{
+	int index;
+
+	for (index = 0; (s != NULL && *(s + index) != '\0'); index++)
+		_putchar(*(s + index));
 }
