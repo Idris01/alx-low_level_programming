fa4db26907a3c2f43770191ce175e9b315630164
root <idrys01@gmail.com>
2023-04-08 01:32:35 -0700

    Add function that prints binary form of a decimal number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary form of a number
+ * @n: long integer number to print binary of
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	const unsigned long int BITS = sizeof(n) * 4;
+	unsigned long int mask = 1 << (BITS - 1);
+	int has_one = 0; /* flag to remove leading zeros */
+	int is_one;
+	unsigned long int index;
+
+	for (index = 0; index < BITS; index++)
+	{
+		is_one = mask & (n << index);
+
+		if (is_one)
+		{
+			has_one = 1;
+			_putchar('1');
+		}
+		else if (has_one) /* prints zero only if preceeded by 1 */
+			_putchar('0');
+	}
+	if (!has_one) /* the number is zero so print only one 0 */
+		_putchar('0');
+
+	_putchar('\n');
+}
