f7977fafd29e12b39e5976107d0aee6becaaa81b
root <idrys01@gmail.com>
2023-05-19 11:38:58 -0700

    handle case of empty list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,11 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->next = NULL;
 	if (temp == NULL)
 	{
-		if (idx == 0)
-		{
-			*h = new;
-			return (new);
-		}
+		free(new);
 		return (NULL);
 	}
 	for (; temp->next != NULL; cnt++, temp = temp->next)
