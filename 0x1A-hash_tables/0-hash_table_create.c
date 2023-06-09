425035246f1e9b6502d44daad1c1a7658a3f4b01
root <idrys01@gmail.com>
2023-06-09 08:10:38 -0700

    fix: add required memory for node

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,9 +8,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
+	hash_node_t **array = NULL;
+	size_t counter = 0;
 
-	table = malloc(sizeof(hash_table_t) * size);
-	if (table == NULL)
+	table = malloc(sizeof(hash_table_t));
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (table == NULL || array == NULL)
 		return (NULL);
+	for (; counter < size; counter++)
+		array[counter] = NULL;
+
+	table->size = size;
+	table->array = array;
 	return (table);
 }
